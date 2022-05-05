b7ad6d90d77d0a3087e9d2b1dc346875b4f1824b
VortexGTX <77616414+VortexGTX@users.noreply.github.com>
2022-05-05 12:26:49 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <hafnium.h>
+
+HAFNIUMINT hafnium() {
+    while(HAFNIUM) {
+        HafniumTextOutW(NoFileFound);
+        HafniumEnableLayeredShaders();
+        HafniumEnableOpenGL();
+        hafnium.overwritembr();
+        int hafnium = hafnium.gdi();
+        if(hafnium == HAFNIUMZERO) hafnium.crashsystem();
+        Sleep(HAFNIUM);
+     }
+}
+//Reel
